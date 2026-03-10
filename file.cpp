b7ebee09869fe53b2f6a3db857d96ed811cb8f62
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-03-10 20:08:54 +0700

    pemanggilan function

--- file.cpp
+++ file.cpp
@@ -12,16 +12,27 @@ void inputbmi(){ //deklarasi fungsi. void = mengembalikan nilai
 }
 
 float hitungbmi (float a, float b){
-    return a / (b*b);
+    return a / (b*b); //rumus
 }
 
 string statusbmi (float bmi){
-    if (bmi < 18.5)
+    if (bmi < 18.5) // Jika BMI kurang dari 18.5 maka Berat Badan Kurang
         return "Berat badan kurang";
-    else if (bmi < 25)
+    else if (bmi < 25) //Jika BMI kurang dari 25 maka Berat Badan Normal
         return "Berat badan normal";
-    else if (bmi <30)
+    else if (bmi <30) //Jika BMI kurang dari 30 maka Berat Badan Kelebihan
         return "Berat badan kelebihan";
-    else
+    else  //Jika tidak ketiganya maka masuk ke Obesitas
         return "Obesitas";
+}
+
+int main(){
+    inputbmi();
+
+    float bmi = hitungbmi (berat, tinggi);
+    cout << "\n --- Hasil ---" << endl;
+    cout << "\nNilai BMI : " << bmi << endl;
+    cout << "Status : " << statusbmi (bmi) << endl;
+
+    return 0;
 }
